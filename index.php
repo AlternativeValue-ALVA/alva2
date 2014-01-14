587c7a977131e521f20f8297c642d0469901dbde
m0k1 <moki.forum@gmail.com>
2014-01-14 05:52:09 +0100

    Preparing for raintpl

--- index.php
+++ index.php
@@ -1,47 +1,59 @@
-<?php
-/*
-UserCake Version: 2.0.2
-http://usercake.com
+<?php 
+/*  <ALVA V2 - ALVA CMS v2>
+    Copyright (C) <2014>  <Momcilo Moki Micanovic>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 */
 
-require_once("models/config.php");
+define('INCLUDE_ALLOWED', true);
+$page = preg_replace("/[^a-zA-Z0-9]+/", "", $_GET['page']);
+$get = preg_replace("/[^a-zA-Z0-9]+/", "", $_GET['get']);
+$get2 = preg_replace("/[^a-zA-Z0-9]+/", "", $_GET['get2']);
+$get3 = preg_replace("/[^a-zA-Z0-9]+/", "", $_GET['get3']);
+$site = 'http://'.$_SERVER['HTTP_HOST'];
+error_reporting(E_ALL ^ E_NOTICE);
+include "app/config.php";
+include "app/detect.php";
+require_once("app/models/config.php");
 if (!securePage($_SERVER['PHP_SELF'])){die();}
-require_once("models/header.php");
+require_once("app/models/header.php");
+if (file_exists('pages/' . $p . '.php')) 
+{
+	include 'pages/' . $p . '.php';
+} 
+else
+{
+	include 'pages/index.php';
+}
 
-echo "
-<body>
-<div id='wrapper'>
-<div id='top'><div id='logo'></div></div>
-<div id='content'>
-<h1>UserCake</h1>
-<h2>2.00</h2>
-<div id='left-nav'>";
-include("left-nav.php");
-
-echo "
-</div>
-<div id='main'>
-<p>Thank you for downloading UserCake. 100% Free and Opensource.</p>
-<p>Copyright (c) 2009-2012</p>
-<p>Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:</p>
-<p>The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.</p>
-<p>THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.</p>
-</div>
-<div id='bottom'></div>
-</div>
-</body>
-</html>";
-
+/*
+if ($page_name=='') 
+{
+	include $browser_t.'/index.html';
+}
+elseif ($page_name=='index.html') 
+{
+	include $browser_t.'/index.html';
+}
+elseif ($page_name=='contact-post.html') 
+{
+	include $browser_t.'/contact.html';
+	include 'app/contact.php';
+}
+else
+{
+	include $browser_t.'/404.html';
+}
+*/
 ?>
